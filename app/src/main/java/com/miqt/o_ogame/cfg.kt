94c9604923a17746aa0a47506531c4bb40feb03b
miqt <miqingtang@163.com>
2017-09-12 17:47:24 +0800

    udp组播调试

--- app/src/main/java/com/miqt/o_ogame/cfg.kt
+++ app/src/main/java/com/miqt/o_ogame/cfg.kt
@@ -6,7 +6,7 @@ package com.miqt.o_ogame
 object cfg {
     //net
     val udp_port = 8842
-    val udp_ip = "239.116.8.0"
+    val udp_ip = "224.0.0.8"
     val tcp_port = 9963
     val rhythm :Long = 3000
 }
